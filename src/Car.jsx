62ea44a6ebd9c385b5faf82db3ec5ac7e4d7d645
TheRogueNinja <shreyassawant018@gmail.com>
2023-08-03 17:39:23 -0400

    wheels physics and colliders added

--- src/Car.jsx
+++ src/Car.jsx
@@ -1,6 +1,9 @@
+import { useBox, useRaycastVehicle } from "@react-three/cannon";
 import { useLoader } from "@react-three/fiber";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useWheels } from "./hooks/useWheels";
+import { WheelDebug } from "./WheelDebug";
 
 export const Car = () => {
     let carModel = useLoader(
@@ -8,12 +11,44 @@ export const Car = () => {
         "/models/car.glb"
     ).scene;
 
-    useEffect(()=>{
-        carModel.scale.set(0.0015, 0.0015, 0.0015);
-        carModel.children[0].position.set(-365,0,-1100);
-    },[carModel]);
+    const position = [-1.5, 0.5, 3];
+    const width = 0.15;
+    const height = 0.07;
+    const front = 0.15;
+    const wheelRadius = 0.05;
 
-    return(
-        <primitive object={carModel} rotation-y={Math.PI} />
+    const chassisBodyArgs = [width, height, front * 2];
+    const [chassisBody, chassisAPI] = useBox(() => ({
+        args: chassisBodyArgs,
+        mass: 150,
+        position,
+    }), useRef(null));
+
+    const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
+
+    const [vehicle, vehicleAPI] = useRaycastVehicle(()=>({
+        chassisBody,
+        wheelInfos,
+        wheels
+    }), useRef(null));
+
+
+    useEffect(() => {
+        carModel.scale.set(0.0015, 0.0015, 0.0015);
+        carModel.children[0].position.set(-365, 0, -1100);
+    }, [carModel]);
+
+    return (
+        // <primitive object={carModel} rotation-y={Math.PI} />
+        <group ref={vehicle} name="vehicle">
+            <mesh ref={chassisBody}>
+                <meshBasicMaterial transparent={true} opacity={0.3} />
+                <boxGeometry args={chassisBodyArgs} />
+            </mesh>
+            <WheelDebug wheelRef={wheels[0]} radius={wheelRadius} />
+            <WheelDebug wheelRef={wheels[1]} radius={wheelRadius} />
+            <WheelDebug wheelRef={wheels[2]} radius={wheelRadius} />
+            <WheelDebug wheelRef={wheels[3]} radius={wheelRadius} />
+        </group>
     )
 }
